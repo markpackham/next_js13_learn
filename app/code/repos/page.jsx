66f3fdcecc0b6de55671b622156f75f9d42800cf
mark <markpackham1@gmail.com>
2023-04-06 16:32:53 +0100

    display my github repos
    https://youtu.be/Y6KDk5iyrYE?t=1925

--- app/code/repos/page.jsx
+++ app/code/repos/page.jsx
@@ -1,7 +1,18 @@
+import Link from "next/link";
+import { FaStar, FaCodeBranch, FaEye } from "react-icons/fa";
+
 async function fetchRepos() {
   const response = await fetch(
-    "https://api.github.com/users/markpackham/repos"
+    "https://api.github.com/users/markpackham/repos",
+    {
+      next: {
+        revalidate: 60,
+      },
+    }
   );
+
+  await new Promise((resolve) => setTimeout(resolve, 1000));
+
   const repos = await response.json();
   return repos;
 }
@@ -9,6 +20,31 @@ async function fetchRepos() {
 const ReposPage = async () => {
   const repos = await fetchRepos();
 
-  return <div>{repos[0].name}</div>;
+  return (
+    <div className="repos-container">
+      <h2>Repositories</h2>
+      <ul className="repo-list">
+        {repos.map((repo) => (
+          <li key={repo.id}>
+            <Link href={`/code/repos/${repo.name}`}>
+              <h3>{repo.name}</h3>
+              <p>{repo.description}</p>
+              <div className="repo-details">
+                <span>
+                  <FaStar /> {repo.stargazers_count}
+                </span>
+                <span>
+                  <FaCodeBranch /> {repo.forks_count}
+                </span>
+                <span>
+                  <FaEye /> {repo.watchers_count}
+                </span>
+              </div>
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 };
 export default ReposPage;

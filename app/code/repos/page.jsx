84db03e58c70d2fc98ec7041fab34ff7b3619962
mark <markpackham1@gmail.com>
2023-04-06 16:23:39 +0100

    fetch my first github repo
    https://youtu.be/Y6KDk5iyrYE?t=1687

--- app/code/repos/page.jsx
+++ app/code/repos/page.jsx
@@ -1,4 +1,14 @@
-const ReposPage = () => {
-  return <div>ReposPage</div>;
+async function fetchRepos() {
+  const response = await fetch(
+    "https://api.github.com/users/markpackham/repos"
+  );
+  const repos = await response.json();
+  return repos;
+}
+
+const ReposPage = async () => {
+  const repos = await fetchRepos();
+
+  return <div>{repos[0].name}</div>;
 };
 export default ReposPage;

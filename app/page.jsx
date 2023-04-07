7dfda3aafbcedb6534200a04d21ae75901faf8ba
mark <markpackham1@gmail.com>
2023-04-07 12:13:15 +0100

    remove obsolete homepage menu

--- app/page.jsx
+++ app/page.jsx
@@ -5,18 +5,6 @@ const HomePage = () => {
   return (
     <>
       <h1>Welcome to the Homepage</h1>
-      <ul>
-        <li>
-          <Link href="/">Home</Link>
-        </li>
-        <li>
-          <Link href="/about">About</Link>
-        </li>
-        <li>
-          <Link href="/about/team">Team</Link>
-        </li>
-      </ul>
-      <br />
       <Courses />
     </>
   );

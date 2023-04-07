a6fe080fbffcee2db7e1bd04f2f2fa8d0a4df922
mark <markpackham1@gmail.com>
2023-04-07 13:44:59 +0100

    setup CourseSearch to get search results from homepage

--- app/page.jsx
+++ app/page.jsx
@@ -3,6 +3,7 @@ import { useState, useEffect } from "react";
 import Link from "next/link";
 import LoadingPage from "./loading";
 import Courses from "./components/Courses";
+import CourseSearch from "./components/CourseSearch";
 
 const HomePage = () => {
   const [courses, setCourses] = useState([]);
@@ -26,6 +27,7 @@ const HomePage = () => {
   return (
     <>
       <h1>Welcome to the Homepage</h1>
+      <CourseSearch getSearchResults={(results) => setCourses(results)} />
       <Courses courses={courses} />
     </>
   );

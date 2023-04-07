e147249e5e2dfe1fe9c03880d00dd13fdac3da78
mark <markpackham1@gmail.com>
2023-04-07 13:12:48 +0100

    useState on homepage
    https://youtu.be/Y6KDk5iyrYE?t=4429

--- app/page.jsx
+++ app/page.jsx
@@ -1,12 +1,16 @@
 "use client";
+import { useState, useEffect } from "react";
 import Link from "next/link";
+import LoadingPage from "./loading";
 import Courses from "./components/Courses";
 
 const HomePage = () => {
+  const [courses, setCourses] = useState([]);
+  const [loading, setLoading] = useState(true);
+
   return (
     <>
       <h1>Welcome to the Homepage</h1>
-      <Courses />
     </>
   );
 };

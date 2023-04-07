39b82535db9fa51d4cb16b5ec46e19f8a18d82fb
mark <markpackham1@gmail.com>
2023-04-07 10:13:28 +0100

    fetch my github directories
    https://youtu.be/Y6KDk5iyrYE?t=2746

--- app/components/RepoDirs.jsx
+++ app/components/RepoDirs.jsx
@@ -1,4 +1,34 @@
-const RepoDirs = () => {
-  return <div>RepoDirs</div>;
+import Link from "next/link";
+
+async function fetchRepoContents(name) {
+  const response = await fetch(
+    `https://api.github.com/repos/markpackham/${name}/contents`,
+    {
+      next: {
+        revalidate: 60,
+      },
+    }
+  );
+  const contents = await response.json();
+  return contents;
+}
+
+const RepoDirs = async ({ name }) => {
+  const contents = await fetchRepoContents(name);
+  // we just want the directories
+  const dirs = contents.filter((content) => content.type === "dir");
+
+  return (
+    <>
+      <h3>Directories</h3>
+      <ul>
+        {dirs.map((dir) => (
+          <li key={dir.path}>
+            <Link href={`/code/repos/${name}/${dir.path}`}>{dir.path}</Link>
+          </li>
+        ))}
+      </ul>
+    </>
+  );
 };
 export default RepoDirs;

4b69aa5737f34396aad786135c830fca36e08119
mark <markpackham1@gmail.com>
2023-04-07 11:57:01 +0100

    finish off course component
    https://youtu.be/Y6KDk5iyrYE?t=3583

--- app/components/Courses.jsx
+++ app/components/Courses.jsx
@@ -1,3 +1,5 @@
+import Link from "next/link";
+
 async function fetchCourses() {
   const response = await fetch("http://localhost:3000/api/courses");
   const courses = await response.json();
@@ -6,6 +8,19 @@ async function fetchCourses() {
 
 const Courses = async () => {
   const coruses = await fetchCourses();
-  return <div>Courses</div>;
+  return (
+    <div className="courses">
+      {courses.map((course) => {
+        <div key={course.id} className="card">
+          <h2>{course.title}</h2>
+          <small>Level: {course.level}</small>
+          <p>{course.description}</p>
+          <Link href={course.link} target="_blank" className="btn">
+            Go to Course
+          </Link>
+        </div>;
+      })}
+    </div>
+  );
 };
 export default Courses;
